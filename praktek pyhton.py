d1e4cdea0335eab1d57f54bbb589cc02c786bd4a
fz-data <zerofadli83@gmail.com>
2025-02-09 19:12:15 +0700

    praktek pyhton.py

---
+++ praktek pyhton.py
@@ -0,0 +1,20 @@
+'''
+ini adalah perintah ibu untuk anaknya
+'''
+print('mulai')
+print('ibu memberi perintah"beli 1 botol susu"')
+print('anak menjawab"ok"')
+print('anak menjawab"ok"')
+print('anak pergi ke toko')
+print('anak pergi ke toko')
+print('anak bertanya "apakah susu ada?"')
+print('anak mengecek harganya cukup')
+print('anak bertanya lagi "apakah ada telur"')
+print('anak membeli satu botol susu dan enam butir telur')
+print('anak pulang kerumah')
+print('anak Menyerahkan hasil kepada ibunya')
+print('selesai')
+
+
+
+
